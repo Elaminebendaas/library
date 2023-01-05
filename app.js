d7984a15f07a1ac32793c20907d71795eb503990
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-01-05 11:17:04 -0500

    Created modal

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ function Book() {
 function addBookToLibrary() {
     // do stuff here
 }
+
+
+
+
+//Opening an closing modal
+let modal = document.getElementById('addBook')
+let openBtn = document.getElementById('openBtn')
+let overlay = document.getElementById('overlay')
+let closeBtn = document.getElementById('closeBtn')
+
+openBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
+
+function openModal() {
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closeModal() {
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
