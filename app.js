5eb3858b857060dc9632fe032377bfdb59f2b8ab
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-01-05 15:32:08 -0500

    Creates Book object from form

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 let myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
     // do stuff here
 }
+
+Book.prototype.Output = function() {
+    console.log(this.title)
+    console.log(this.author)
+    console.log(this.pages)
+    console.log(this.read)
+}
+
+let addBookBtn = document.getElementById('addBookBtn')
+let titleForm = document.getElementById('title')
+let authorForm = document.getElementById('author')
+let pagesForm = document.getElementById('pages')
+let readForm = document.getElementById('read')
+addBookBtn.addEventListener('click', newBook)
+
+function newBook() {
+    console.log(title)
+    const book = new Book(title.value, author.value, pages.value, read.checked)
+    event.preventDefault()
+    book.Output()
+    closeModal()
+}
+
 
 
 
